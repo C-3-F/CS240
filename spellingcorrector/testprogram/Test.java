8be853e3a3c1064afc5412f2ea8fa345a91c4cad
c3farr <c3farr@gmail.com>
2023-01-15 00:00:10 -0700

    start spelling corrector

---
+++ spellingcorrector/testprogram/Test.java
@@ -0,0 +1,11 @@
+package testprogram;
+
+class Test {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        for (var i = 0; i < 10; i++){
+            System.out.printf("Test Number: %d",i);
+        }
+    }
+}
